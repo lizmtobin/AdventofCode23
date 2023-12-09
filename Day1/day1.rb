69c39e20a0356fdd92c6c9a5fb1a46ee295a2a4f
liz tobin <lizmtobin@gmail.com>
2023-12-09 14:38:30 +0000

    [day1] add special case for adjoining number words

--- Day1/day1.rb
+++ Day1/day1.rb
@@ -3,14 +3,45 @@ lines = File.readlines('Day1/input.txt').map(&:chomp)
 NAMED_NUMBERS = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].freeze
 pattern = /(#{NAMED_NUMBERS.join('|')})/
 
+SPECIAL_CASES = {
+  "oneight"     => "18",
+   "twone"      => "21",
+   "threeight"  => "38",
+   "fiveight"    => "58",
+   "sevenine"   => "79",
+   "eightwo"    => "82",
+   "eighthree"  => "83",
+   "nineight"   => "98"
+}
+
+def generate_adjoining_words
+  named_num = NAMED_NUMBERS - ["zero"]
+  combinations = []
+  named_num.each do |first_word|
+    named_num.each do |second_word|
+      if first_word[-1] == second_word[0]
+        combined_word = first_word + second_word[1..]
+        combinations << combined_word
+      end
+    end
+  end
+  combinations.uniq
+end
+
 def word_to_number(word)
   NAMED_NUMBERS.index(word).to_s
 end
 
 def digitise_input(lines, pattern)
+  # Handle special cases first
   lines.map do |str| 
-    str.gsub(pattern) do |word| 
-        word_to_number(word) 
+    special_handled = str.dup
+      SPECIAL_CASES.each do |word, number|
+      special_handled.gsub!(word, number)
+    end
+
+      special_handled.gsub(pattern) do |word| 
+      word_to_number(word) 
       end
    end
 end 
@@ -18,7 +49,7 @@ end
 def collect_first_and_last_num(lines, pattern)
   digitised_lines = digitise_input(lines, pattern)
   digitised_lines.map do |numbers|
-    digits = numbers.scan(/\d/) 
+    digits = numbers.scan(/\d/)
     if digits.empty?
       nil 
     elsif digits.length == 1
@@ -34,6 +65,7 @@ def sum_digits(lines, pattern)
   digit_pairs.map(&:to_i).sum
 end
 
+# generate_adjoining_words - returns an array of all possible combinations of adjoining words
+
 total = sum_digits(lines, pattern)
 p total
-# p sum # => 55123 The sum of all the calibrations
