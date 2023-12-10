85c22bad8d75adcc22b6beec9be6a7c0d37fd044
liz tobin <lizmtobin@gmail.com>
2023-12-10 15:02:45 +0000

    [day2] wrong end of stick

--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -1,36 +1,35 @@
 require_relative '../Day2/day2.rb'
 
-RSpec.describe 'build_totals_hash' do
-  let(:test_lines) do
-    [
-      "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-    ]
-  end
+describe 'valid_game_count' do
+ 
+  describe 'is_set_valid?' do
+    it 'returns true when the set is valid' do
+      expect(is_set_valid?('2 red, 3 green, 4 blue')).to eq(true)
+    end
 
-  describe '#process_game_line' do
-    it 'correctly processes a single line of game data' do
-      line = test_lines.first
-      puts line
-      expected_output = { "Game 1" => {"blue" => 9, "red" => 5, "green" => 4} }
-      expect(process_game_line(line)).to eq(expected_output)
+    it 'returns false when the set is invalid' do
+      expect(is_set_valid?('5 red, 6 green, 50 blue')).to eq(false)
     end
   end
 
-  describe '#build_totals_hash' do
-    it 'correctly processes multiple lines of game data' do
-      expected_output = {
-      "Game 1" => {"blue" => 9, "red" => 5, "green" => 4},
-      "Game 2" => {"blue" => 6, "green" => 6, "red" => 1},
-      "Game 3" => {"green" => 26, "blue" => 11, "red" => 25},
-      "Game 4" => {"green" => 7, "red" => 23, "blue" => 21},
-      "Game 5" => {"red" => 7, "blue" => 3, "green" => 5}
-    }
-      expect(build_totals_hash(test_lines)).to eq(expected_output)
+  describe 'process_game_line' do
+    it 'returns the game score when all sets are valid' do
+      expect(process_game_line('Game 1: 2 green, 6 blue, 7 red; 12 green, 6 blue, 3 red')).to eq(1)
+    end
+
+    it 'returns 0 when at least one set is invalid' do
+      expect(process_game_line('Game 7: 19 green, 2 red, 5 blue; 4 blue, 8 green; 2 red, 2 green, 9 blue')).to eq(0)
+    end
+  end
+
+  describe 'find_total_valid_games' do
+    it 'returns the total count by adding game ids' do
+      lines = [
+        'Game 1: 2 red, 3 green, 4 blue; 1 red, 2 green, 3 blue',
+        'Game 3: 5 red, 6 green, 7 blue; 2 red, 3 green, 4 blue',
+        'Game 5: 1 red, 1 green, 1 blue; 2 red, 2 green, 2 blue'
+      ]
+      expect(find_total_valid_games(lines)).to eq(9)
     end
   end
 end
-
