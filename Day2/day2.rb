dcb21668a8510e32a39c09d4ac122fe94d72167e
liz tobin <lizmtobin@gmail.com>
2023-12-10 15:56:50 +0000

    [day2] calculate power of max colors total

--- Day2/day2.rb
+++ Day2/day2.rb
@@ -1,3 +1,5 @@
+# GAME 1 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 def is_set_valid?(set)
   color_counts = Hash.new(0)
   set.split(', ').each do |color_count|
@@ -24,6 +26,39 @@ def find_total_valid_games(lines)
   valid_games_count
 end
 
+# GAME 2 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+def highest_color_value_per_game(lines)
+  all_games = {}
+
+  lines.each do |line|
+    game, sets = line.split(': ')
+    all_games[game] ||= {} 
+
+    sets.split('; ').each do |set|
+      set.split(', ').each do |color_count|
+        count, color = color_count.split
+        count = count.to_i
+        all_games[game][color] = [all_games[game][color] || 0, count].max
+      end
+    end
+  end
+
+  all_games
+end
+
+def find_power_of_color(lines)
+  total_power = 0
+  high_colors = highest_color_value_per_game(lines)
+  high_colors.each do |game, colors|
+    total_power += colors['red'] * colors['green'] * colors['blue']
+  end
+  total_power
+end
+
 lines = File.readlines("Day2/input.txt", chomp: true)
-total_valid_games = find_total_valid_games(lines)
-puts "Total valid games: #{total_valid_games}"
+# total_valid_games = find_total_valid_games(lines)
+# puts "Total valid games: #{total_valid_games}"
+
+# highest_color_value_per_game(lines)
+p find_power_of_color(lines)
