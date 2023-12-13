0bda51f2d17a56bb462a0a3c7dc223cf6b4bdb98
liz tobin <lizmtobin@gmail.com>
2023-12-13 15:02:30 +0000

    [day3] part 2

--- Day3/day3.rb
+++ Day3/day3.rb
@@ -1,3 +1,15 @@
+require "set"
+# rules 
+# Part 1
+# 1. Find all numbers that are adjacent to a symbol
+# 2. Sum all of those numbers
+# 3. Return the sum
+
+# Part 2
+# 1. Find gears. A gear is any * symbol that is adjacent to exactly two part numbers. 
+# 2. The gear ratio is the result of multiplying those two numbers together.
+# 3. find the gear ratio of every gear and add them all up
+
 def is_symbol?(char)
   !char.match?(/[0-9a-zA-Z.]/)
 end
@@ -61,6 +73,65 @@ def sum_part_numbers_from_file(filename)
   sum
 end
 
+# part 2
 
-filename = 'Day3/input.txt'
-puts sum_part_numbers_from_file(filename)
+input =  <<~INPUT
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+INPUT
+puts input
+# input = File.read("Day3/input.txt")
+rows = input.each_line.map(&:chomp)
+# rows = DATA.readlines.map(&:chomp)
+
+syms = []
+
+rows.each_with_index do |row, x|
+  row.each_char.with_index do |char, y|
+    # next if char == "."
+    # next if char =~ /\d/
+    # p [char, x, y]
+    syms << [x, y] if char == "*"
+  end
+end
+
+sum = 0
+syms.each do |(x, y)|
+  number_starts = Set.new
+
+  [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1],
+  ].each do |(dx, dy)|
+    nx = x + dx
+    ny = y + dy
+    if rows[nx][ny] =~ /\d/
+      nny = ny
+      while rows[nx][nny - 1] =~ /\d/
+        nny -= 1
+      end
+      number_starts << [nx, nny]
+      puts number_starts
+    end
+  end
+
+  if number_starts.length == 2
+    
+    require "pry"; binding.pry
+    result = number_starts.map do |(x, y)|
+      rows[x][y..].to_i
+    end.inject(:*)
+    sum += result
+  end
+end
+
+p sum
